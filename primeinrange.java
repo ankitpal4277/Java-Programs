854bfc1a657a2f93a0f5f88b0557159e2810c8c6
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-07-17 18:48:25 +0530

    Prime numbers in a range

---
+++ primeinrange.java
@@ -0,0 +1,26 @@
+public class primeinrange {
+
+    public static boolean isPrime(int n){
+        for(int i = 2; i < n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void primeInRange(int n){
+        for(int i = 2; i <=n; i++){
+            if(isPrime(i)){
+                System.out.println(i + "");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        // System.out.println(isPrime(8));
+
+        primeInRange(20);
+    }
+}
