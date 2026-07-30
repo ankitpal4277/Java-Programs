0e6c27ad208efbafbf8a369d6607c5b52d091bbb
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-07-30 18:56:33 +0530

    Creation of Array in Java

---
+++ Arrays/creation.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class creation {
+    public static void main(String args[]){
+        int marks[] = new int[50]; // Arrays are Static in size.
+
+        int numbers[] = {1, 2, 3};
+
+        String fruits[] = {"Apple", "Mango", "Banana"};
+    }
+}
